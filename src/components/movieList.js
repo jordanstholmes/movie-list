4f1444a2bca9f48e59e1a74cd61d31f1cabb4bee
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-09 23:20:26 -0700

    Populate movieList with mapped react elements

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -2,9 +2,13 @@ var React = require('react');
 var MovieListItem = require('./movieListItem.js');
 
 module.exports = function MovieList(props) {
+  // var movies = props.movieData.map((movie) => {
+  //   return 
+  // });
+
   return (
     <div className='container movie-list-container'>
-      <MovieListItem title={props.movieData[0].title} />
+      { props.movieData.map((movie) => <MovieListItem title={movie.title} />) }
     </div>
   );
 }
